ffc70308fd825570d96681dec10f2a9096d425a7
jega1312 <192710907+jega1312@users.noreply.github.com>
2026-09-04 13:18:55 +0800

    chore: added background overlay toggle

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const navBtn = document.querySelector("#nav-btn");
 const navImg = document.querySelector("#nav-img");
+const overlayBg = document.querySelector("#overlay");
 
 navBtn.addEventListener("click", () => {
   navImg.classList.toggle("clicked");
+  navBtn.classList.toggle("opened");
 
   if (navImg.classList.contains("clicked")) {
     navImg.src = "./assets/images/icon-close.svg";
   } else {
     navImg.src = "./assets/images/icon-menu.svg";
+  }
+
+  if (navBtn.classList.contains("opened")) {
+    overlayBg.classList.remove("hidden");
+  } else {
+    overlayBg.classList.add("hidden");
   }
 });
