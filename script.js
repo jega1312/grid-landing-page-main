2aad180c93073c74cb74431318cf8e328d810278
jega1312 <192710907+jega1312@users.noreply.github.com>
2026-09-04 13:51:18 +0800

    chore: added side navigation menu for desktop

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const navBtn = document.querySelector("#nav-btn");
 const navImg = document.querySelector("#nav-img");
 const overlayBg = document.querySelector("#overlay");
+const navTab = document.querySelector("#nav-tab");
 
 navBtn.addEventListener("click", () => {
   navImg.classList.toggle("clicked");
   navBtn.classList.toggle("opened");
+  navTab.classList.toggle("active");
 
   if (navImg.classList.contains("clicked")) {
     navImg.src = "./assets/images/icon-close.svg";
@@ -16,5 +18,11 @@ navBtn.addEventListener("click", () => {
     overlayBg.classList.remove("hidden");
   } else {
     overlayBg.classList.add("hidden");
+  }
+
+  if (navBtn.classList.contains("opened")) {
+    navTab.classList.remove("hidden");
+  } else {
+    navTab.classList.add("hidden");
   }
 });
